6fa208e5215dc5a2922f10d25a3458b5a2418b35
Bishakh Ghosh <ghoshbishakh@gmail.com>
2019-03-30 13:42:50 +0530

    Fix metadata_manager issues and add tests

---
+++ test_metadata_manager.py
@@ -0,0 +1,106 @@
+import unittest
+import metadata_manager
+import os
+import shutil
+
+subscriber1 = 'test_subuscriber_1'
+subscriber2 = 'test_subuscriber_2'
+topic1 = 'test_topic_1'
+topic2 = 'test_topic_2'
+
+class TestTopics(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        if os.path.isfile(metadata_manager.metadata_file_name):
+            print('Backing up original metadata file...')
+            shutil.copyfile(metadata_manager.metadata_file_name, metadata_manager.metadata_file_name+ '.BAK')
+            cls.backup = True
+            os.remove(metadata_manager.metadata_file_name)
+        else:
+            cls.backup = False
+
+    def test_add_topic(self):
+        t = metadata_manager.add_topic(topic1)
+        self.assertListEqual(t, [topic1])
+        t = metadata_manager.add_topic(topic2)
+        self.assertListEqual(t, [topic1, topic2])
+
+    def test_get_topic(self):
+        metadata_manager.add_topic(topic1)
+        t = metadata_manager.add_topic(topic2)
+        t2 = metadata_manager.get_topics()
+        self.assertListEqual(t, t2)
+    
+    @classmethod
+    def tearDownClass(cls):
+        if cls.backup:
+            print('Removing backup ...')
+            shutil.copyfile(metadata_manager.metadata_file_name+ '.BAK', metadata_manager.metadata_file_name)
+            cls.backup = False
+            os.remove(metadata_manager.metadata_file_name+ '.BAK')
+        else:
+            try:
+                os.remove(metadata_manager.metadata_file_name)
+            except:
+                pass
+
+
+class TestSubscriptions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        if os.path.isfile(metadata_manager.metadata_file_name):
+            print('Backing up original metadata file...')
+            shutil.copyfile(metadata_manager.metadata_file_name, metadata_manager.metadata_file_name+ '.BAK')
+            cls.backup = True
+            os.remove(metadata_manager.metadata_file_name)
+        else:
+            cls.backup = False
+
+    def test_add_subscription(self):
+        s = metadata_manager.add_subscription(subscriber1, topic1)
+        self.assertListEqual(s, [topic1])
+        s = metadata_manager.add_subscription(subscriber1, topic2)
+        self.assertListEqual(s, [topic1, topic2])
+        s = metadata_manager.add_subscription(subscriber2, topic2)
+        self.assertListEqual(s, [topic2])
+
+
+    def test_check_subscription(self):
+        s = metadata_manager.add_subscription(subscriber1, topic1)
+        s = metadata_manager.add_subscription(subscriber1, topic2)
+        s = metadata_manager.add_subscription(subscriber2, topic2)
+        self.assertTrue(metadata_manager.check_subscription(subscriber1, topic1))
+        self.assertTrue(metadata_manager.check_subscription(subscriber1, topic2))
+        self.assertTrue(metadata_manager.check_subscription(subscriber2, topic2))
+        self.assertFalse(metadata_manager.check_subscription(subscriber2, topic1))
+    
+    @classmethod
+    def tearDownClass(cls):
+        if cls.backup:
+            print('Removing backup ...')
+            shutil.copyfile(metadata_manager.metadata_file_name+ '.BAK', metadata_manager.metadata_file_name)
+            cls.backup = False
+            os.remove(metadata_manager.metadata_file_name+ '.BAK')
+        else:
+            try:
+                os.remove(metadata_manager.metadata_file_name)
+            except:
+                pass
+
+
+    # def get_topic(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
