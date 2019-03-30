bcae3f5d58d730dcd785a0fcaa130662c9c81dec
Surya <suryateja836@gmail.com>
2019-03-30 23:54:31 +0530

    Update createtopic and publish

--- publisher_client.py
+++ publisher_client.py
@@ -1,6 +1,6 @@
 import requests
 import argparse
-
+from strgen import StringGenerator
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-id','--pub_name',type= str, help='Name of the publisher', required=True)
@@ -12,26 +12,37 @@ args = parser.parse_args()
 publisher_name = args.pub_name
 service_addr = args.server
 service_port = args.port
-service_url = service_addr + ':' + str(service_port)
+service_url = 'http://' + service_addr + ':' + str(service_port)
 # print(args.server)
 
 
 def create_topic(topic_name):
     data = {'pub_name':publisher_name,'topic_name':topic_name}
-    r = requests.post(service_url + '/createtopic/', data=data)
+    r = requests.post(service_url + '/createtopic', json=data)
     if r.status_code == 200 :
         print('POST successful')
     else :
-        print('HTTP Respone code : ' + r.status_code + '  ' + r.reason)
+        print('HTTP Respone code : ' + str(r.status_code) + '  ' + r.reason)
     print(r.text)
 
 
 def publish_to_topic(topic_name, msgs):
-    data = {'pub_name':publisher_name,'payload':'msgs'}
+    data = {'pub_name':publisher_name,'payload':msgs}
     r = requests.post(service_url + '/publish/' + topic_name, json=data)
     if r.status_code == 200 :
         print('POST successful')
     else :
-        print('HTTP Respone code : ' + r.status_code + '  ' + r.reason)
+        print('HTTP Respone code : ' + str(r.status_code) + '  ' + r.reason)
     print(r.text)
 
+def main():
+    create_topic('Apache')
+    # msgs = StringGenerator('[\\w\\p\\d]{20}').render_list(1000,unique=True)
+    # publish_to_topic('Apache',msgs)
+    # msgs = StringGenerator('[\\w\\p\\d]{20}').render_list(1000,unique=True)
+    # publish_to_topic('Apache',msgs)
+    # msgs = StringGenerator('[\\w\\p\\d]{20}').render_list(1000,unique=True)
+    # publish_to_topic('Apache',msgs)
+
+
+main()
