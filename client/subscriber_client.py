465da6bde4fd486a1d19ca5d1c5e9e5dd4f068fc
Surya <suryateja836@gmail.com>
2019-04-02 00:26:21 +0530

    Update both clients

--- client/subscriber_client.py
+++ client/subscriber_client.py
@@ -12,35 +12,44 @@ args = parser.parse_args()
 subscriber_name = args.sub_name
 service_addr = args.server
 service_port = args.port
-service_url = service_addr + ':' + str(service_port)
+service_url = 'http://' + service_addr + ':' + str(service_port)
 
 topic_offsets = {}
 
 def subscribe_to(topic_name):
     data = {'sub_name':subscriber_name,'topic_name': topic_name}
-    r = requests.post(service_url + '/subscribe',data=data)
+    r = requests.post(service_url + '/subscribe',json=data)
     if r.status_code == 200 :
         print("POST successful")
-        topic_offsets[topic_name] = 0
+        topic_offsets[topic_name] = 1
     else :
-        print("HTTP Response code : " + r.status_code + "  " + r.reason)
+        print("HTTP Response code : " + str(r.status_code) + "  " + r.reason)
     print('Response from server : ' + r.text)
 
 def unsubscribe_from(topic_name):
     data = {'sub_name':subscriber_name,'topic_name': topic_name}
-    r = requests.post(service_url + '/unsubscribe',data=data)
+    r = requests.post(service_url + '/unsubscribe',json=data)
     if r.status_code == 200 :
         print("POST successful")
         del topic_offsets[topic_name]
     else :
-        print("HTTP Response code : " + r.status_code + "  " + r.reason)
+        print("HTTP Response code : " + str(r.status_code) + "  " + r.reason)
     print('Response from server : ' + r.text)
 
 def read_data(topic_name):
-    r = requests.get(service_url + '/readfrom/' + topic_name + '/' + str(topic_offsets[topic_name]))
+    print(service_url + '/readfrom/' + topic_name + '/' + str(topic_offsets[topic_name]))
+    r = requests.get(service_url + '/readfrom/' + topic_name + '/' + str(topic_offsets[topic_name]),params = {'sub_name':subscriber_name})
     # read data
     if r.status_code == 200 :
-        print("POST successful")
+        print("READ successful")
+        topic_offsets[topic_name] += 1
     else :
-        print("HTTP Response code : " + r.status_code + "  " + r.reason)
+        print("HTTP Response code : " + str(r.status_code) + "  " + r.reason)
     print('Response from server : ' + r.text)
+
+def main():
+    subscribe_to('Apache')
+    for _ in range(1,10):
+        read_data('Apache')
+
+main()
